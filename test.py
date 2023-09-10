ad79874273627c7e014773b1c391adeec21eb7f2
Raz <razshinar1@gmail.com>
2023-09-10 18:13:22 +0300

    Commit 3 add requirements delete test

--- test.py
+++
@@ -1,4 +0,0 @@
-import pdfkit
-path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
-config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
-pdfkit.from_file("templates/home.html", "out.pdf", configuration=config, options={"enable-local-file-access": ""})
